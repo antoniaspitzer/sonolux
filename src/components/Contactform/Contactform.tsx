0566a7443b754732798d11cf84ac05f864296380
antoniaspitzer <s2410238084@fhooe.at>
2026-07-26 14:59:10 +0200

    first draft of contact formular

--- src/components/Contactform/Contactform.tsx
+++ src/components/Contactform/Contactform.tsx
@@ -1,23 +1,62 @@
-import Link from "next/link";
-import Image from "next/image";
 import styles from "./Contactform.module.css";
 
 export default function Contactform() {
   return (
-    <>
-        <h1>Kontakt</h1>
+    <div className={styles.wrapper}>
+      <h1>Kontakt</h1>
 
-        <div className={styles.wrapper}>
-            <div className={styles.left}>
+      <form action="#" method="POST" className={styles.form}>
 
-            </div>
+        <div className={styles.row}>
+          <div className={styles.inputGroup}>
+            <label htmlFor="vorname">Vorname</label>
+            <input 
+              type="text" 
+              name="vorname" 
+              id="vorname" 
+              placeholder="Jane" 
+              required 
+            />
+          </div>
 
-            <div className={styles.right}>
-
-            </div>
-
+          <div className={styles.inputGroup}>
+            <label htmlFor="nachname">Nachname</label>
+            <input 
+              type="text" 
+              name="nachname" 
+              id="nachname" 
+              placeholder="Smitherton" 
+              required 
+            />
+          </div>
         </div>
 
-    </>
+        <div className={styles.inputGroup}>
+          <label htmlFor="email">E-Mail Adresse</label>
+          <input 
+            type="email" 
+            name="email" 
+            id="email" 
+            placeholder="email@janesfakedomain.net" 
+            required 
+          />
+        </div>
+
+        <div className={styles.inputGroup}>
+          <label htmlFor="nachricht">Nachricht</label>
+          <textarea 
+            name="nachricht" 
+            id="nachricht" 
+            rows={6}
+            placeholder="Enter your question or message"
+            required
+          ></textarea>
+        </div>
+        
+        <div className={styles.buttonWrapper}>
+          <button type="submit" className="orange-button">Submit</button>
+        </div>
+      </form>
+    </div>
   );
-};
+}
